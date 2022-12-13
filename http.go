811aaaf76d015cd98ccfc16c76c487bb0aa30984
Andrew Peliza <andrewpeliza@hotmail.com>
2022-12-13 15:37:27 +0000

    feat: http and custom interface to go with it.

--- http.go
+++ http.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func http_playground() {
 	resp, err := http.Get("http://google.com/")
@@ -12,5 +15,16 @@ func http_playground() {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
-	fmt.Println(resp)
+
+	lw := logWriter{}
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
 }
