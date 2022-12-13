036665853421f636db56575b906830d257b3231a
Andrew Peliza <andrewpeliza@hotmail.com>
2022-12-13 14:15:30 +0000

    feat: interfaces and basic http req

--- interfaces.go
+++ interfaces.go
@@ -1,5 +1,33 @@
 package main
 
-func interfaces() {
+import "fmt"
 
+type bot interface {
+	getGreeting() string
+	// getGreeting(string, int) (string, error)
+	// getBotVersion() float64
+	// respondToUser(string) string
+}
+type englishBot struct{}
+type spanishBot struct{}
+
+func interfaces() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func (englishBot) getGreeting() string {
+	// VERY custom logic for generating an english greeting
+	return "Hi there!"
+}
+
+func (spanishBot) getGreeting() string {
+	return "Hola!"
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
